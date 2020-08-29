406d076c833946b9d69f6a446788fd953d3ba527
ArthurLeandro <arthur.leandro11@gmail.com>
2020-08-29 08:34:31 -0300

    Working on the views. Basic structure finished.

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var config = require('./server/configure'),
+	app = express();
+app.set("port", process.env.PORT || 3300);
+app.set("views", __dirname + "/views");
+app = config(app);
+
+app.get("/", function (req, res) {
+	res.send("Hello World");
+});
+app.listen(app.get("port"), function () {
+	console.log('Server up and running: http://localhost:' + app.get("port"));
+});
